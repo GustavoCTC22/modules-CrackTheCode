4caf5f908d9bb363337e0e2f509f93f62baa9377
Gustavout22 <gustavougartetorres@gmail.com>
2024-06-13 21:11:24 -0500

    add: final version

---
+++ tasks-app/dom.js
@@ -0,0 +1,36 @@
+import { getTasks, deleteTasks, addTasks } from "./tasks.js";
+
+//obtener elementos DOM
+const buttonDelete = document.querySelector("#delete-tasks");
+const buttonAdd = document.querySelector("#add-task");
+const inputTask = document.querySelector("#new-task");
+const ul = document.querySelector("#tasks");
+
+const showTasks = () => {
+  ul.innerHTML = "";
+  const tasks = getTasks();
+
+  ul.innerHTML = `
+  ${tasks.map((ele) => `<li>${ele}</li>`).join("")}
+  `;
+};
+
+const handleTasks = (_event) => {
+  const newTask = inputTask.value;
+
+  if (newTask === "") return;
+
+  addTasks(newTask);
+  inputTask.value = "";
+  showTasks();
+};
+
+const handleDelete = (_event) => {
+  ul.innerHTML = "";
+  deleteTasks();
+};
+
+buttonDelete.addEventListener("click", handleDelete);
+buttonAdd.addEventListener("click", handleTasks);
+
+showTasks();
--- tasks-app/tasks.js
+++ tasks-app/tasks.js
@@ -1,50 +1,14 @@
-//obtener elementos DOM
-const buttonDelete = document.querySelector("#delete-tasks");
-const buttonAdd = document.querySelector("#add-task");
-const inputTask = document.querySelector("#new-task");
-const ul = document.querySelector("#tasks");
 //Primer paso
-const tasks = ["Comer", "Dormir", "Desayunar"];
+export const tasks = ["Comer", "Dormir", "Desayunar"];
 
-const deleteTasks = () => {
+export const deleteTasks = () => {
   tasks.splice(0, tasks.length);
 };
 
-const addTasks = (task) => {
+export const addTasks = (task) => {
   tasks.push(task);
 };
 
-const getTasks = () => {
+export const getTasks = () => {
   return tasks;
 };
-
-//========================================
-
-const showTasks = () => {
-  ul.innerHTML = "";
-  const tasks = getTasks();
-
-  ul.innerHTML = `
-  ${tasks.map((ele) => `<li>${ele}</li>`).join("")}
-  `;
-};
-
-const handleTasks = (_event) => {
-  const newTask = inputTask.value;
-
-  if (newTask === "") return;
-
-  addTasks(newTask);
-  inputTask.value = "";
-  showTasks();
-};
-
-const handleDelete = (_event) => {
-  ul.innerHTML = "";
-  deleteTasks();
-};
-
-buttonDelete.addEventListener("click", handleDelete);
-buttonAdd.addEventListener("click", handleTasks);
-
-showTasks();
